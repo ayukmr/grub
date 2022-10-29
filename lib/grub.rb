b1ef4c0bb7b2faa5153d826a6fdd4de474195b8d
Ayush Kumar <github@ayushk.xyz>
2022-10-29 13:03:04 -0700

    Reword comments

--- lib/grub.rb
+++ lib/grub.rb
@@ -1,13 +1,15 @@
+# libraries
 require 'clipboard'
 require 'erb'
 require 'html/pipeline'
 require 'rouge'
 require 'webrick'
 
-# grub modules
+# modules
 module Grub
 end
 
+# library
 require 'grub/utils'
 require 'grub/rouge'
 require 'grub/server'
